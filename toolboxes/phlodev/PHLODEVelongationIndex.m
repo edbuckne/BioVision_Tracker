10c90c5b0515736a6b45e8797e20e5edd68e5f54
edbuckne <edbuckne@ncsu.edu>
2020-02-04 17:52:57 -0500

    Updated 2/4/2020

---
+++ toolboxes/phlodev/PHLODEVelongationIndex.m
@@ -0,0 +1,54 @@
+function PHLODEVelongationIndex(varargin)
+% phlodevElongationIndex.m calculates all elongation index measurements for
+% the specimen specified by spm and saves it into the specimen directory
+% under the file elongationIndex.mat in the variable ei.
+% Inputs:
+%   spm (int): The specimen number id
+% Outputs:
+%   ei (vec double): Elongation index for each region detected in clInfo
+defaultSPM = 'all';
+
+p = inputParser();
+addParameter(p, 'SPM', defaultSPM);  % The number of the specimen
+
+parse(p, varargin{:});
+
+load('./data_config'); % Load the configuration data
+
+if ischar(p.Results.SPM) % Determine if all specimen, or just 1
+    if strcmp(p.Results.SPM, 'all')
+        spmlist = tSpm(:, 1)';
+    else
+        error(['I''m not sure what you mean by ' p.Results.SPM ', this should be ''all'' or a number']);
+    end
+elseif isnumeric(p.Results.SPM)
+    spmlist = round(p.Results.SPM);
+else
+    error('Check your SPM variable. It should be ''all'' or a number');
+end
+
+for spm = spmlist
+    spmDir = ['SPM' num2str(spm, '%.2u')]; % Initialize variables based on already known information
+    disp(spmDir);
+    [clInfo, timeArray] = loadclInfo(spm);
+    ei = zeros(size(clInfo, 1), 1);
+    
+    spmIndex = tSpm(:, 1) == spm; % Get the range of time stamps for this specimen
+    tRange = tSpm(spmIndex, 2):tSpm(spmIndex, 3);
+    
+    for t = tRange  % Go through each time stamp in the experiment
+        I = microImInputRaw(spm, t, 1, 1);  % load the 3d fluorescence image into the I variable
+        disp(['Calculating elongation index for time stamp ' num2str(t)]);
+        for i = timeArray(t, 1):timeArray(t, 2)  % Go through each detected region in this time stamp
+            try
+                ei(i) = elongationIndex(spm, i, I);  % Calculate the elongation index
+            catch
+                ei(i) = -1;
+            end
+        end
+    end
+    
+    save([spmDir '/elongationIndex'], 'ei');
+end
+end
+
